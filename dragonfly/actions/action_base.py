3d12594b27adbbe87fea809d7c31d31d8e31bb77
Hawkeye Parker <parker.hawkeye@gmail.com>
2015-11-03 18:03:10 -0800

    Add exec_info to some logging plus a few more logging statements to get stack trace during debugging

--- dragonfly/actions/action_base.py
+++ dragonfly/actions/action_base.py
@@ -3,18 +3,18 @@
 # (c) Copyright 2007, 2008 by Christo Butcher
 # Licensed under the LGPL.
 #
-#   Dragonfly is free software: you can redistribute it and/or modify it 
-#   under the terms of the GNU Lesser General Public License as published 
-#   by the Free Software Foundation, either version 3 of the License, or 
+#   Dragonfly is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   Dragonfly is distributed in the hope that it will be useful, but 
-#   WITHOUT ANY WARRANTY; without even the implied warranty of 
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+#   Dragonfly is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #   Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU Lesser General Public 
-#   License along with Dragonfly.  If not, see 
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with Dragonfly.  If not, see
 #   <http://www.gnu.org/licenses/>.
 #
 
@@ -86,7 +86,7 @@ class ActionBase(object):
             if self._execute(data) == False:
                 raise ActionError(str(self))
         except ActionError, e:
-            self._log_exec.error("Execution failed: %s" % e)
+            self._log_exec.error("Execution failed: %s" % e, exc_info=True)
             return False
         return True
 
--- dragonfly/engines/backend_natlink/engine.py
+++ dragonfly/engines/backend_natlink/engine.py
@@ -3,18 +3,18 @@
 # (c) Copyright 2007, 2008 by Christo Butcher
 # Licensed under the LGPL.
 #
-#   Dragonfly is free software: you can redistribute it and/or modify it 
-#   under the terms of the GNU Lesser General Public License as published 
-#   by the Free Software Foundation, either version 3 of the License, or 
+#   Dragonfly is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   Dragonfly is distributed in the hope that it will be useful, but 
-#   WITHOUT ANY WARRANTY; without even the implied warranty of 
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+#   Dragonfly is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #   Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU Lesser General Public 
-#   License along with Dragonfly.  If not, see 
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with Dragonfly.  If not, see
 #   <http://www.gnu.org/licenses/>.
 #
 
@@ -202,9 +202,10 @@ class NatlinkEngine(EngineBase):
                                % (words, e))
         try:
             self.natlink.recognitionMimic(prepared_words)
-        except self.natlink.MimicFailed:
-            raise MimicFailure("No matching rule found for words %r."
-                               % (prepared_words,))
+        except self.natlink.MimicFailed, e:
+            msg = "Error: No matching rule found for words: %r." % (prepared_words,)
+            self._log.error(msg + ": " + str(e), exc_info=True)
+            raise MimicFailure(msg, e)
 
     def speak(self, text):
         """ Speak the given *text* using text-to-speech. """
